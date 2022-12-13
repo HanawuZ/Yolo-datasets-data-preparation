fe123d115c9083c7e5ed6b0e78f1934cca260a57
HanawuZ <b6302245@gmail.com>
2022-12-13 13:14:49 +0700

    add data-rename function

--- data-rename.py
+++ data-rename.py
@@ -9,7 +9,7 @@ root = pathlib.Path(__file__).parent
 path_datasets = root/"datasets"
 
 
-def image_rename(src,name):
+def datasets_rename(src,name="example"):
     """ Function for renaming all images and label files in directory by input name.
     ### Parameters
     - src : source of file directory.
@@ -19,14 +19,21 @@ def image_rename(src,name):
     """
     images_path = src/"images"
     labels_path = src/"labels"
-    print(os.listdir(images_path))
-    # Iterated all files
+
+    # Iterated all images files
+    for count, filename in enumerate(os.listdir(images_path)):
+        img_dst = f"{name}_{str(count)}.png"
+        img_src = f"{images_path}/{filename}"
+        rename_img_dst = f"{images_path}/{img_dst}"
+
+        # Rename images file
+        os.rename(img_src, rename_img_dst)
 
 
-
-    pass
-
-datasets_name = input("Enter datasets directory's name: ")
-if datasets_name not in os.listdir():
-    print(f"There is no directory name {datasets_name} in {root}")
-    pass
+while True:
+    datasets_name = input("Enter datasets directory's name: ")
+    if datasets_name not in os.listdir(path_datasets):
+        print(f"There is no directory name {datasets_name} in {root}")
+    else :
+        datasets_rename(path_datasets/datasets_name)
+        break
